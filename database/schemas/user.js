df5ff23fe6d91a10d7d78cfb7c9f2c7ed42f2809
Kriz <christian.munoz1@mail.udp.cl>
2021-03-28 13:34:07 -0300

    estructura base

---
+++ database/schemas/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const BankSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    intereses: {
+        type: Array,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Bancos", BankSchema);
