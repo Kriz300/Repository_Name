ee38a9481d834c7a3f45dc8dff6944e9f0010f4b
Kriz300 <christian.munoz1@mail.udp.cl>
2021-03-28 18:41:59 -0300

    prueba bd

--- routes/test.js
+++ routes/test.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const exel = require("@formulajs/formulajs");
 const mongoose = require('mongoose');
-const bank = require('../database/schemas/bank.js');
+const bank = require('../database/schemas/bank');
 
 
 router.get("/", async (req, res) => {
     const banks = await bank.find({},(err) => {
         if (err) console.error(err)
+        return;
     });
     res.send(banks);
 });
