a3f60e5d3397d9f17e9a6c9cf2861dca2883ef53
Kriz300 <christian.munoz1@mail.udp.cl>
2021-03-28 19:08:38 -0300

    prueba bd

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-const bancos = require("./bancos");
+const test = require("./test");
 
-router.use("/bancos", bancos);
+router.use("/test", test);
 
 module.exports = router;
