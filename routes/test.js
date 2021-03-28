21c56d2f2f862b2b6090d134420b783850e5f46f
Kriz300 <christian.munoz1@mail.udp.cl>
2021-03-28 18:43:51 -0300

    prueba bd

--- routes/test.js
+++ routes/test.js
@@ -5,8 +5,9 @@ const bank = require('../database/schemas/bank');
 
 
 router.get("/", async (req, res) => {
-    const banks = await bank.find({},(err) => {
+    const banks = await bank.find({},(err, bancos) => {
         if (err) console.error(err)
+        console.log(bancos);
         return;
     });
     res.send(banks);
