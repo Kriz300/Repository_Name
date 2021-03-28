89bb8d496fba46e5c874098fe02fa88e3142fcf7
Kriz300 <christian.munoz1@mail.udp.cl>
2021-03-28 18:51:44 -0300

    prueba bd

--- routes/test.js
+++ routes/test.js
@@ -10,6 +10,14 @@ router.get("/", async (req, res) => {
         console.log(bancos);
         return;
     });
+    banco = new bank({
+        nombre: "Prueba",
+        VIP: [1, 2, 3]
+    });
+
+    banco.save()
+        .then(result => console.log(result))
+        .catch(err => console.error(err));
     res.send(banks);
 });
 
